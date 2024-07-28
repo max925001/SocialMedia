dbe9b1b5976fac5feb56c11bb67d13c9a0b265b7
max925001 <shivam969684@gmail.com>
2024-07-28 10:05:02 -0700

    solving error

--- server/middleware/multer.Middleware.js
+++ server/middleware/multer.Middleware.js
@@ -1,35 +1,31 @@
-import path from 'path'
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
 
-import multer from 'multer'
+// Construct __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const upload = multer({
-dest:"uploads/",
-limits: {fileSize: 60* 1024 * 1024}, //50 mb upload
-storage: multer.diskStorage({
-    destination:'uploads/',
-    filename:(_req ,file,cb) =>{
-cb(null , file.originalname)
-    },
-}),
-fileFilter:(_req,file,cb) =>{
-    let ext = path.extname(file.originalname)
+// Function to ensure the directory exists
+const ensureDirectoryExistence = (dirPath) => {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+};
 
-    if(
-         
-        ext !== ".jpg" &&
-        ext !== ".jpeg" &&
-        ext !== ".png" &&
-        ext !== ".mp4" &&
-        ext !== ".mkv" 
+// Set up storage with multer
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    const uploadPath = path.join(__dirname, 'uploads');
+    ensureDirectoryExistence(uploadPath);
+    cb(null, uploadPath);
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
 
-
-    ){
-cb(new Error(`Unsupported file type ${ext} `),false )
-
-return
-    }
-    cb(null , true)
-},
-
-})
-export default upload
+export const upload = multer({
+  storage,
+});
